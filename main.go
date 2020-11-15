6becf7ecffe4eaf04b717f8b2df43cd7522355c7
Niels Koster <n.koster@portavita.eu>
2020-11-15 15:19:41 +0100

    Use function literal for sleep in go routine and pass a value into

--- main.go
+++ main.go
@@ -28,13 +28,15 @@ func main() {
 	// }
 
 	for link := range c {
-		go checkLink(link, c)
+		go func(l string) {
+			time.Sleep(2 * time.Second)
+			checkLink(l, c)
+		}(link)
 	}
 
 }
 
 func checkLink(link string, c chan string) {
-	time.Sleep(2 * time.Second)
 	resp, err := http.Get(link)
 	if err != nil {
 		c <- link
